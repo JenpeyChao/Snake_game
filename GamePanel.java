1c5d0ce793daf45ed8b16587c73853615107e3de
aaron <redskin.rox@gmail.com>
2024-05-28 15:12:56 -0400

    adding comments

--- GamePanel.java
+++ GamePanel.java
@@ -107,23 +107,27 @@ public class GamePanel extends JPanel implements ActionListener{
 	public void draw(Graphics graphics) {
 		
 		if (running) {
+			//draws the food
 			graphics.setColor(new Color(210, 115, 90));
 			graphics.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
 
+			//draws the obstacles
 			for(int i = 0; i < numObstacles; i++){
 				graphics.setColor(Color.CYAN);
 				graphics.fillOval(obstaclesX[i], obstaclesY[i], UNIT_SIZE, UNIT_SIZE);
 			}
 			
-
+			//draws the head
 			graphics.setColor(Color.white);
 			graphics.fillRect(x[0], y[0], UNIT_SIZE, UNIT_SIZE);
-			
+
+			//draws the body
 			for (int i = 1; i < length; i++) {
 				graphics.setColor(new Color(40, 200, 150));
 				graphics.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 			}
 			
+			//draws the scoreboard
 			graphics.setColor(Color.white);
 			graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 25));
 			FontMetrics metrics = getFontMetrics(graphics.getFont());
@@ -139,19 +143,23 @@ public class GamePanel extends JPanel implements ActionListener{
 		foodY = random.nextInt((int)(HEIGHT / UNIT_SIZE))*UNIT_SIZE;
 	}
 	public void addObstacles(){
+		//creates the # of obstacles
 		for(int i = 0; i < numObstacles; i++){
 			boolean made;
 			do{
+				//creates the coords for the obstacle
 				made = false;
 				int x = random.nextInt((int)(WIDTH / UNIT_SIZE))*UNIT_SIZE;
 				int y = random.nextInt((int)(WIDTH / UNIT_SIZE))*UNIT_SIZE;
 				//make sure the obstacle that was made isnt already made
 				for(int j = 0; j<i ; j++){
+					//if it is then it stops and restarts the loop
 					if (x == obstaclesX[j] && y == obstaclesY[j]) {
 						made = true;
 						break;
 					}
 				}
+				
 				obstaclesX[i] = x; //X
 				obstaclesY[i] = y; //Y
 				//need to check if the spot for the obstacle isnt on the food
@@ -169,6 +177,7 @@ public class GamePanel extends JPanel implements ActionListener{
 				running = false;
 			}
 		}
+		//makes sure the head of the snake doesnt run into a obstacle
 		for (int i = 0; i < numObstacles; i++){
 			if (obstaclesX[i] == x[i] && obstaclesY[i] == y[i]){
 				running = false;
@@ -186,6 +195,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	public void gameOver(Graphics graphics) {
+		//game over screen
 		graphics.setColor(Color.red);
 		graphics.setFont(new Font("Sans serif", Font.ROMAN_BASELINE, 50));
 		FontMetrics metrics = getFontMetrics(graphics.getFont());
